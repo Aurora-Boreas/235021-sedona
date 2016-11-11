e14c602446843002d6ffba6fd72eae4a02d11bd2
Aurora-Boreas <borga78@list.ru>
2016-11-11 20:45:10 +0400

    Декоративные элементы #4
    Оживила поп-ап, исправила соц кнопки

---
+++ js/main.js
@@ -0,0 +1,27 @@
+  var link = document.querySelector(".btn-order");
+  var popup = document.querySelector(".form-popup");
+  var date_in = popup.querySelector("[name=date-in]");
+  var date_out = popup.querySelector("[name=date-out]");
+  
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var popup = document.querySelector(".form-popup");
+    popup.classList.toggle("form-popup-show");
+    date_in.focus();
+  });
+
+  popup.addEventListener("submit", function(event) {
+    if (!date_in.value || !date_out.value) {
+        event.preventDefault();
+        return alert("Введите дату заезда и дату выезда!");
+    }
+  }); 
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (popup.classList.contains("form-popup-show")) {
+        popup.classList.remove("form-popup-show");
+      }
+    }
+  });
